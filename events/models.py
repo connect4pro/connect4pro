f017d147851ed2d194330bb3e6d834f302eb42cb
Sashkewic <sashkewicha@gmail.com>
2021-05-20 12:08:21 +0600

    Обнаружены и исправлены пару неточностей в модели приложения Events

--- events/models.py
+++ events/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django_resized import ResizedImageField
-
 # Create your models here.
 
 class Event(models.Model):
     """Мероприятие (тема, дата, время, сумма (вход))"""
     name = models.CharField(max_length = 100)
-    event_image = ResizedImageField(size=[250, 250], upload_to=f'images/events_images/{name}-%d%m%Y', blank=True,
-                                   null=True)
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length = 100)
     sum = models.PositiveIntegerField()
+    event_image = ResizedImageField(size=[250, 250], upload_to=f'images/event_images/{name}-%d%m%Y', blank=True,
+                                   null=True)
 
     def __str__(self):
         return f'{self.name}'
