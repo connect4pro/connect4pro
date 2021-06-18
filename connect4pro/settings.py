3be130a4fbdcc42c4df3c741ccaf3c1290cc299e
Sashkewic <sashkewicha@gmail.com>
2021-06-18 22:29:29 +0600

    Изменена БД с sqlite на postgres

--- connect4pro/settings.py
+++ connect4pro/settings.py
@@ -93,24 +93,16 @@ WSGI_APPLICATION = 'connect4pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'connect4pro',
-#         'USER': 'drf',
-#         'PASSWORD': '120666',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'connect4pro',
+        'USER': 'drf',
+        'PASSWORD': '120666',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
