a46ea2a0f0e5ee4e2a5b66e0f6c76becf38dd907
Дмитрий Макаров <maggot8950@gmail.com>
2021-06-22 23:32:55 +0600

    Update admin.py

--- adverts/admin.py
+++ adverts/admin.py
@@ -4,13 +4,14 @@ from adverts.models import BusinessAdvert, Category, ProviderAdvert
 admin.site.register(Category)
 
 
+
 @admin.register(BusinessAdvert)
 class BusinessAdvertAdmin(admin.ModelAdmin):
-    list_display = ['user', 'title', 'price', 'currency', 'category', 'completed']
+    list_display = ['user', 'title' ,'price', 'currency', 'category', 'completed']
     list_filter = ['currency', 'category__name', 'completed']
 
 
 @admin.register(ProviderAdvert)
 class ProviderAdvertAdmin(admin.ModelAdmin):
-    list_display = ['user', 'title', 'price', 'currency', 'category']
+    list_display = ['user', 'title' ,'price', 'currency', 'category']
     list_filter = ['currency', 'category__name']
