1a5b02adda4988ec4f45149de4858555201b79de
Дмитрий Макаров <maggot8950@gmail.com>
2021-06-22 23:28:04 +0600

    Изменено отображение в админке для моделей: Объявления(оба типа), Профили(оба типа)

--- adverts/admin.py
+++ adverts/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-
-# Register your models here.
 from adverts.models import BusinessAdvert, Category, ProviderAdvert
 
 admin.site.register(Category)
-admin.site.register(BusinessAdvert)
-admin.site.register(ProviderAdvert)
+
+
+
+@admin.register(BusinessAdvert)
+class BusinessAdvertAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title' ,'price', 'currency', 'category', 'completed']
+    list_filter = ['currency', 'category__name', 'completed']
+
+
+@admin.register(ProviderAdvert)
+class ProviderAdvertAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title' ,'price', 'currency', 'category']
+    list_filter = ['currency', 'category__name']
