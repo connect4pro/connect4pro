b34d5e74fc00ef4fa372614a0d7e0f48f89b4f36
Дмитрий Макаров <maggot8950@gmail.com>
2021-07-14 22:48:34 +0600

    Доделана оплата премиум аккаунта

--- payments/urls.py
+++ payments/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from payments.views import pay_result, pay_premium, pay_success
+from payments.views import pay_premium, pay_success, pay_result, pay_failure
 
 app_name = 'payments'
 
 urlpatterns = [
-    path('paybox-result', pay_result, name='paybox_result'),
     path('paybox', pay_premium, name='paybox_init'),
-    path('paybox-success', pay_success, name='paybox-success'),
+    path('paybox-success', pay_success, name='paybox_success'),
+    path('paybox-result', pay_result, name='paybox_result'),
+    path('paybox-failure', pay_failure, name='paybox_failure'),
 ]
