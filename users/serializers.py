d101860b2fde3485294cff339cc609fc73f98a06
Дмитрий Макаров <maggot8950@gmail.com>
2021-07-16 01:00:38 +0600

    Правка аватара

--- users/serializers.py
+++ users/serializers.py
@@ -32,10 +32,10 @@ class Connect4ProUserBPSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True)
     business_profile = BusinessProfileSerializer(required=True)
     is_premium = serializers.BooleanField(read_only=True)
+    avatar = serializers.ImageField(required=False)
 
     def create(self, validated_data):
         profile_data = validated_data.pop('business_profile')
-
         sector_data = profile_data.pop('sector')
         if validated_data['password'] != validated_data['password2']:
             raise ValidationError('Passwords must match')
@@ -47,7 +47,6 @@ class Connect4ProUserBPSerializer(serializers.ModelSerializer):
                 facebook=validated_data['facebook'],
                 instagram=validated_data['instagram'],
                 site=validated_data['site'],
-                avatar=validated_data['avatar']
             )
         profile = BusinessProfile.objects.create(user=user, **profile_data)
 
@@ -60,7 +59,8 @@ class Connect4ProUserBPSerializer(serializers.ModelSerializer):
     class Meta:
         model = Connect4ProUser
         fields = (
-            'id', 'email', 'password', 'password2', 'avatar', 'company_name', 'facebook', 'instagram', 'site', 'is_premium',
+            'id', 'email', 'password', 'password2', 'avatar', 'company_name', 'facebook', 'instagram', 'site',
+            'is_premium',
             'business_profile')
 
 
@@ -80,6 +80,7 @@ class Connect4ProUserPPSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True)
     provider_profile = ProviderProfileSerializer(required=True)
     is_premium = serializers.BooleanField(read_only=True)
+    avatar = serializers.ImageField(required=False)
 
     def create(self, validated_data):
         profile_data = validated_data.pop('provider_profile')
@@ -93,7 +94,6 @@ class Connect4ProUserPPSerializer(serializers.ModelSerializer):
                 facebook=validated_data['facebook'],
                 instagram=validated_data['instagram'],
                 site=validated_data['site'],
-                avatar=validated_data['avatar']
             )
         ProviderProfile.objects.create(user=user, **profile_data)
         return user
@@ -101,7 +101,8 @@ class Connect4ProUserPPSerializer(serializers.ModelSerializer):
     class Meta:
         model = Connect4ProUser
         fields = (
-            'id', 'email', 'password', 'password2', 'company_name', 'facebook', 'instagram', 'site', 'is_premium',
+            'id', 'email', 'password', 'password2', 'avatar', 'company_name', 'facebook', 'instagram', 'site',
+            'is_premium',
             'provider_profile')
 
 
@@ -110,7 +111,7 @@ class UpdateBusinessProfile(serializers.ModelSerializer):
 
     class Meta:
         model = Connect4ProUser
-        fields = ('company_name', 'facebook', 'instagram', 'site', 'business_profile')
+        fields = ('company_name', 'facebook', 'avatar', 'instagram', 'site', 'business_profile')
 
     def update(self, instance, validated_data):
         instance.company_name = validated_data['company_name']
@@ -128,7 +129,7 @@ class UpdateProviderProfile(serializers.ModelSerializer):
 
     class Meta:
         model = Connect4ProUser
-        fields = ('company_name', 'facebook', 'instagram', 'site', 'provider_profile')
+        fields = ('company_name', 'facebook', 'avatar', 'instagram', 'site', 'provider_profile')
 
     def update(self, instance, validated_data):
         instance.company_name = validated_data['company_name']
