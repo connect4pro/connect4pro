7d4de74abb8e7af92147117735757ff5cee2ef39
Дмитрий Макаров <maggot8950@gmail.com>
2021-07-21 20:53:31 +0600

    Update settings.py

--- connect4pro/settings.py
+++ connect4pro/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 # TODO: Закрыть доступ для посторонних айпи
-ALLOWED_HOSTS = ['http://localhost:8000', 'http://localhost:3000', 'http://94.228.120.61/', 'http://94.228.120.61', '94.228.120.61']
+ALLOWED_HOSTS = ['http://localhost:8000', 'http://localhost:3000', 'http://94.228.120.61/', 'http://94.228.120.61', '94.228.120.61', 'localhost', '127.0.0.1']
 
 # Application definition
 
