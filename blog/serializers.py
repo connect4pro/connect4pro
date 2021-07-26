8d0166becb41608df198c7b45119cc8ab523b73b
Дмитрий Макаров <maggot8950@gmail.com>
2021-07-26 20:09:31 +0600

    Добавлены комментарии к блогу

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from blog.models import BlogPost
+from blog.models import BlogPost, BlogComment
+
+
+class BlogCommentSerializer(serializers.ModelSerializer):
+    '''Blog comment serialize'''
+
+    class Meta:
+        model = BlogComment
+        fields = ['id', 'post', 'commentator_text', 'commentator_name', 'commentator_email']
+        read_only_fields = ('user',)
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = BlogPost
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'created', 'post_image', 'blog_comment']
+        read_only_fields = ('user',)
+        depth = 1
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from blog.views import BlogPostList
+from blog.views import BlogPostList, BlogDetail, BlogCommentsList, BlogCommentCreate
 
 urlpatterns = [
     path('api/blogposts', BlogPostList.as_view()),
+    path('api/blogposts/detail/<id>', BlogDetail.as_view(), name='blog_create'),
+    path('api/blogposts/comments', BlogCommentsList.as_view(), name='blog_comments'),
+    path('api/blogposts/comments/create_comment', BlogCommentCreate.as_view(), name='blog_comments_create'),
 ]
