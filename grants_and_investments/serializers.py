462c2e37a45dec6056e750705569aa31867d26f0
Дмитрий Макаров <maggot8950@gmail.com>
2021-07-27 17:23:25 +0600

    Добавлены поля grants and investments

--- grants_and_investments/serializers.py
+++ grants_and_investments/serializers.py
@@ -2,20 +2,26 @@ from django.db.models import fields
 from rest_framework import serializers
 from grants_and_investments.models import Grant, Investment, GrantInvestmentComment
 
+
 class GrantSerializer(serializers.ModelSerializer):
     """Grant serialize"""
+
     class Meta:
         model = Grant
-        fields = ('id','grant_name', 'grant_sum', 'currency', 'grant_deadline', 'grant_description')
+        fields = (
+        'id', 'grant_name', 'grant_sum', 'currency', 'grant_deadline', 'grant_description', 'location', 'period')
         read_only_fields = ('user',)
 
 
 class InvestmentSerializer(serializers.ModelSerializer):
     """Investment serialize"""
+
     class Meta:
         model = Investment
-        fields = ('id', 'invest_name', 'invest_sum', 'currency', 'invest_deadline', 'invest_description')
+        fields = (
+        'id', 'invest_name', 'invest_sum', 'currency', 'invest_deadline', 'invest_description', 'location', 'period')
         read_only_fields = ('user',)
+
 
 class GrantInvestmentCommentSerializer(serializers.ModelSerializer):
     """Grant and Investment comment serialize"""
