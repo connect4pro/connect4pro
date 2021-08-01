42be67c07e7d008f7c9f11ed6c340b2c7e9ef1d7
Дмитрий Макаров <maggot8950@gmail.com>
2021-08-01 19:11:56 +0600

    Добавлена проверка премиум статуса и отключение его

--- connect4pro/celery.py
+++ connect4pro/celery.py
@@ -12,6 +12,10 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'send-report-every-single-minute': {
         'task': 'newsletter.tasks.send_newsletter',
-        'schedule': crontab(minute='25', day_of_week='3,5'),  # change to `crontab(minute=0, hour=0)` if you want it to run daily at midnight
+        'schedule': crontab(minute='25', hour='23', day_of_week='3,5'),  # change to `crontab(minute=0, hour=0)` if you want it to run daily at midnight
+    },
+    'check_premium_every_day': {
+        'task': 'payments.tasks.check_and_off_premium',
+        'schedule': crontab(minute='0', hour='23'),
     },
 }
