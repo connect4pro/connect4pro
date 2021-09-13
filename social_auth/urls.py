2be5271a3da7c83ed5cd88722c518c5acbb709db
Дмитрий Макаров <maggot8950@gmail.com>
2021-09-13 19:44:59 +0600

    Изменены urls/social_auth

--- social_auth/urls.py
+++ social_auth/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import GoogleSocialAuthView, FacebookSocialAuthView
 
 urlpatterns = [
-    path('api/google/', GoogleSocialAuthView.as_view()),
-    path('api/facebook/', FacebookSocialAuthView.as_view()),
+    path('api/google', GoogleSocialAuthView.as_view()),
+    path('api/facebook', FacebookSocialAuthView.as_view()),
 ]
