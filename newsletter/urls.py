d0c3ef73c2ee5f36d6f8fe95a04f79c4dd7b2337
Дмитрий Макаров <maggot8950@gmail.com>
2021-10-06 19:54:01 +0600

    Unsubscribe newsletter

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from newsletter.views import NewsletterSubscribe
+from newsletter.views import NewsletterSubscribe, NewsletterUnsubscribe
+
 app_name = 'newsletter'
 urlpatterns = [
     path('api/subscribe/', NewsletterSubscribe.as_view(), name='newsletter_subscribe'),
+    path('api/unsubscribe/', NewsletterUnsubscribe.as_view(), name='newsletter_unsubscribe'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView
 
 from newsletter.models import Contacts
 from newsletter.serializers import ContactsSerializer
 
 
 class NewsletterSubscribe(CreateAPIView):
+    serializer_class = ContactsSerializer
+
+
+class NewsletterUnsubscribe(DestroyAPIView):
     serializer_class = ContactsSerializer
