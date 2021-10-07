5dcdb0b745cf0033383b1df4cb0f5f2c9ad9e8d8
Дмитрий Макаров <maggot8950@gmail.com>
2021-10-07 09:35:31 +0600

    Парвки урлов

--- newsletter/views.py
+++ newsletter/views.py
@@ -15,5 +15,5 @@ class NewsletterUnsubscribe(DestroyAPIView):
     lookup_field = 'email'
 
     def get_queryset(self):
-        queryset = Contacts.objects.all()
+        queryset = Contacts.objects.all().last()
         return queryset
