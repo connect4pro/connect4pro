f031531b3449c51764db856e3815e3671e706aca
Дмитрий Макаров <maggot8950@gmail.com>
2021-10-08 17:41:33 +0600

    testimage

--- testimage/urls.py
+++ testimage/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from testimage.views import ImageView
 
 urlpatterns = [
-    path('api/image_create/', ImageView.as_view(), name='image_create'),
+    path('api/image_create', ImageView.as_view(), name='image_create'),
 ]
