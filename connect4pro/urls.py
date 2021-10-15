3465b777f22add28a6d245405800dbeeb94c9031
Дмитрий Макаров <maggot8950@gmail.com>
2021-10-16 01:24:31 +0600

    paybox

--- connect4pro/urls.py
+++ connect4pro/urls.py
@@ -12,6 +12,7 @@ from . import settings
 from .yasg import urlpatterns as yasg_urls
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
